717b086fca4bb71fc5f6d163d1f0f6327fbbe9f8
Ehidiamen Ikhimiukor <ehi_god@yahoo.com>
2025-03-28 14:58:31 +0100

    Changed to cloud

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "BetterMoney",
+  title: "BetterStocks",
   description: "An AI project by Team Beta of Divverse Group",
 };
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 export async function fetchStockReport(filename) {
-    let url = `http://localhost:8000/reports/${filename}`;
+    let url = `https://stock-analyzer-all.onrender.com/reports/${filename}`;
     console.log(url);
-    const response = await fetch(`http://localhost:8000/reports/${filename}`);
+    const response = await fetch(`https://stock-analyzer-all.onrender.com/reports/${filename}`);
   
     if (!response.ok) {
       throw new Error("Report not found");
